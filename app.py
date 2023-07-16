39e4e85a95f6ca396da53f1b3724a5e617008bd6
Rickythegreat1 <134174492+Rickythegreat1@users.noreply.github.com>
2023-07-16 13:59:51 -0400

    aider: Added a /hello endpoint to the flask app that returns "Hello, World!".
    # Aider chat conversation:
    
    # Context:
    USER: where is app.py located?
    USER: make a flask app with a /hello endpoint that returns hello world

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-// entire file content ...
-// ... goes in between
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
